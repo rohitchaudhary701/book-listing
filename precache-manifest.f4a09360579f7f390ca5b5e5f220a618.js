402a8297363ae1fd467b45284241cfab920ced9c
rohitchaudhary701 <rohitchaudhary701@gmail.com>
2019-02-01 19:16:42 +0530

    Updates

---
+++ precache-manifest.f4a09360579f7f390ca5b5e5f220a618.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f509064fb6b36ca407d",
+    "url": "/book-listing/static/js/runtime~main.5f509064.js"
+  },
+  {
+    "revision": "f5fea77af45c0499cb45",
+    "url": "/book-listing/static/js/main.f5fea77a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/book-listing/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f5fea77af45c0499cb45",
+    "url": "/book-listing/static/css/main.767d572b.chunk.css"
+  },
+  {
+    "revision": "7080c0079c53c60c41e6e9a6dacaef3e",
+    "url": "/book-listing/index.html"
+  }
+];
